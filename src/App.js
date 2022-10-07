b7dddc8433732b49ec5dcfa64598372793d36ea4
chathurperera <chathuraperera007@gmail.com>
2022-10-08 00:06:34 +0530

    Layout components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./components/Login";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public/>} />
+        <Route path="login" element={<Login/>} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
